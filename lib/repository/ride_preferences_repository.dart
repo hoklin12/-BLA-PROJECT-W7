728dded89f1a88c5cd41f5bb36f308da0dcd661c
hoklin12 <153165021+hoklin12@users.noreply.github.com>
2025-03-27 13:04:46 +0700

    Delete lib/repository directory

--- lib/repository/ride_preferences_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../model/ride/ride_pref.dart';
-
-abstract class RidePreferencesRepository {
-  Future<List<RidePreference>> getPastPreferences();
-
-  Future<void> addPreference(RidePreference preference);
-}
